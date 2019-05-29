9748983bb14b571da2c00ffefabc9d7b01f65377
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-05-29 23:02:09 +0900

    Making some changes in gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,43 @@
-var gulp = require('gulp');
-var runSequence = require('run-sequence').use(gulp);
-var pug = require('gulp-pug');
-var del = require('del');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
-var plumber = require('gulp-plumber');
-var gutil = require('gulp-util');
-var babel = require('gulp-babel');
+const gulp = require('gulp');
+const runSequence = require('run-sequence').use(gulp);
+const pug = require('gulp-pug');
+const del = require('del');
+const sass = require('gulp-sass');
+const watch = require('gulp-watch');
+const plumber = require('gulp-plumber');
+const gutil = require('gulp-util');
+const babel = require('gulp-babel');
 
-gulp.task('watch',['pug','sass','js','assets'], function(){
+gulp.task('watch',['pug','sass','js','assets'], () => {
     gulp.watch(['src/scss/*.scss','src/scss/**/*.scss'],['sass']);
     gulp.watch(['src/pug/*.pug','src/pug/components/*.pug'],['pug']);
     gulp.watch(['src/assets/**/*'],['assets']);
     gulp.watch(['src/js/*'],['js']);
 });
 
-gulp.task('pug',function(){
-    return gulp.src('src/pug/*.pug')
-        .pipe(plumber())
-        .pipe(pug())
-        .on('error', gutil.log)
-        .pipe(gulp.dest('docs'))
-});
+gulp.task('pug',() => gulp.src('src/pug/*.pug')
+    .pipe(plumber())
+    .pipe(pug())
+    .on('error', gutil.log)
+    .pipe(gulp.dest('docs')));
 
-gulp.task('js',function(){
-    return gulp.src('src/js/*.js')
-        .pipe(plumber())
-        .pipe(babel({presets: ['env']}))
-        .on('error', gutil.log)
-        .pipe(gulp.dest('docs/assets/js'))
-});
+gulp.task('js', () => gulp.src('src/js/*.js')
+    .pipe(plumber())
+    .pipe(babel({presets: ['env']}))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('docs/assets/js')));
 
-gulp.task('sass',function(){
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(plumber())
-        .pipe(sass())
-        .on('error', gutil.log)
-        .pipe(gulp.dest('docs/assets/css'));
-});
+gulp.task('sass', () => gulp.src('src/scss/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .on('error', gutil.log)
+    .pipe(gulp.dest('docs/assets/css')));
 
-gulp.task('assets',function(){
-    return gulp.src('src/assets/**/*')
-        .pipe(gulp.dest('docs/assets'));
-});
+gulp.task('assets', () => gulp.src('src/assets/**/*').pipe(gulp.dest('docs/assets')));
 
-gulp.task('clean', function() {
-    return del.sync(['docs/**', '!docs', '!docs/bower_components', '!docs/bower_components/**']);
-});
+gulp.task('clean', () => del.sync(['docs/**', '!docs', '!docs/bower_components', '!docs/bower_components/**']));
 
-gulp.task('cssvendor',function() {
-    return gulp.src('src/scss/vendor/**/*')
-        .pipe(gulp.dest('docs/assets/css/vendor'));
-});
+gulp.task('cssvendor', () => gulp.src('src/scss/vendor/**/*')
+        .pipe(gulp.dest('docs/assets/css/vendor')));
 
-gulp.task('default', function(){
-    runSequence('clean', 'assets', 'cssvendor', ['pug', 'sass', 'js'])
-});
+gulp.task('default', () => runSequence('clean', 'assets', 'cssvendor', ['pug', 'sass', 'js']));

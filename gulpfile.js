6b37f3ca5265a7c2744e95ce798fdceb6636e7ef
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-05-30 22:48:08 +0900

    Changing gulpfile.js file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,41 +2,33 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const del = require('del');
 const sass = require('gulp-sass');
-const watch = require('gulp-watch');
 const plumber = require('gulp-plumber');
 const gutil = require('gulp-util');
 const babel = require('gulp-babel');
 
-gulp.task('watch', gulp.series(gulp.parallel('pug','sass','js','assets'), () => {
-    gulp.watch(['src/scss/*.scss','src/scss/**/*.scss'], ['sass']);
-    gulp.watch(['src/pug/*.pug','src/pug/components/*.pug'], ['pug']);
-    gulp.watch(['src/assets/**/*'], ['assets']);
-    gulp.watch(['src/js/*'], ['js']);
-}));
-
-gulp.task('pug', () => gulp.src('src/pug/*.pug')
+gulp.task('pug', async () => gulp.src('src/pug/*.pug')
     .pipe(plumber())
     .pipe(pug())
     .on('error', gutil.log)
     .pipe(gulp.dest('docs')));
 
-gulp.task('js', () => gulp.src('src/js/*.js')
+gulp.task('js', async () => gulp.src('src/js/*.js')
     .pipe(plumber())
     .pipe(babel({presets: ['env']}))
     .on('error', gutil.log)
     .pipe(gulp.dest('docs/assets/js')));
 
-gulp.task('sass', () => gulp.src('src/scss/**/*.scss')
+gulp.task('sass', async () => gulp.src('src/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .on('error', gutil.log)
     .pipe(gulp.dest('docs/assets/css')));
 
-gulp.task('assets', () => gulp.src('src/assets/**/*').pipe(gulp.dest('docs/assets')));
+gulp.task('assets', async () => gulp.src('src/assets/**/*').pipe(gulp.dest('docs/assets')));
 
-gulp.task('clean', () => del.sync(['docs/**', '!docs', '!docs/bower_components', '!docs/bower_components/**']));
+gulp.task('clean', async () => del.sync(['docs/**', '!docs', '!docs/bower_components', '!docs/bower_components/**']));
 
-gulp.task('cssvendor', () => gulp.src('src/scss/vendor/**/*')
+gulp.task('cssvendor', async () => gulp.src('src/scss/vendor/**/*')
         .pipe(gulp.dest('docs/assets/css/vendor')));
 
 gulp.task('default', gulp.series('clean', 'assets', 'cssvendor', gulp.parallel('pug', 'sass', 'js')));

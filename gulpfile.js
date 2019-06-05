5bd94a65811fed280939a9dea2ece1ed4ba47bd6
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-06-05 22:51:01 +0900

    Changing gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,28 @@ const plumber = require('gulp-plumber');
 const gutil = require('gulp-util');
 const babel = require('gulp-babel');
 
+const DEPLOYMENT_FOLDER = 'public';
+
 gulp.task('pug', async () => gulp.src('src/pug/*.pug')
     .pipe(plumber())
     .pipe(pug())
     .on('error', gutil.log)
-    .pipe(gulp.dest('docs')));
+    .pipe(gulp.dest(DEPLOYMENT_FOLDER)));
 
 gulp.task('js', async () => gulp.src('src/js/*.js')
     .pipe(plumber())
     .pipe(babel({presets: ['@babel/env']}))
     .on('error', gutil.log)
-    .pipe(gulp.dest('docs/assets/js')));
+    .pipe(gulp.dest(`${DEPLOYMENT_FOLDER}/assets/js`)));
 
 gulp.task('sass', async () => gulp.src('src/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .on('error', gutil.log)
-    .pipe(gulp.dest('docs/assets/css')));
+    .pipe(gulp.dest(`${DEPLOYMENT_FOLDER}/assets/css`)));
 
-gulp.task('assets', async () => gulp.src('src/assets/**/*').pipe(gulp.dest('docs/assets')));
+gulp.task('assets', async () => gulp.src('src/assets/**/*').pipe(gulp.dest(`${DEPLOYMENT_FOLDER}/assets`)));
 
-gulp.task('clean', async () => del.sync(['docs/**', '!docs', '!docs/bower_components', '!docs/bower_components/**']));
+gulp.task('clean', async () => del.sync([`${DEPLOYMENT_FOLDER}/**`, `!${DEPLOYMENT_FOLDER}`, `!${DEPLOYMENT_FOLDER}/bower_components`, `!${DEPLOYMENT_FOLDER}/bower_components/**`]));
 
 gulp.task('default', gulp.series('clean', 'assets', gulp.parallel('pug', 'sass', 'js')));

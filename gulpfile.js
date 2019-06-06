4d62064d57090c7911ead2f9870e5400366deb8d
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-06-07 00:30:32 +0900

    Changing gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,6 @@ const DEV_DEPLOYMENT_FOLDER = 'docs';
 
 let CURRENT_DEPLOYMENT_FOLDER;
 
-const cleanTask = {};
-cleanTask[CI_DEPLOYMENT_FOLDER] = 'ci-clean';
-cleanTask[DEV_DEPLOYMENT_FOLDER] = 'dev-clean';
-
 const defineDeploymentFolder = f => cb => {
     CURRENT_DEPLOYMENT_FOLDER = f;
     console.log(`Deployment folder: ${CURRENT_DEPLOYMENT_FOLDER}`);
@@ -41,16 +37,16 @@ gulp.task('sass', async () => gulp.src('src/scss/**/*.scss')
 
 gulp.task('assets', async () => gulp.src('src/assets/**/*').pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets`)));
 
-gulp.task('dev-clean', async () => del.sync([`${CURRENT_DEPLOYMENT_FOLDER}/assets`, `${CURRENT_DEPLOYMENT_FOLDER}/index.html`]));
+gulp.task('clean', async () => del.sync([`${CURRENT_DEPLOYMENT_FOLDER}/assets`, `${CURRENT_DEPLOYMENT_FOLDER}/index.html`]));
 
-gulp.task('ci-clean', async () => del.sync(['**']));
+gulp.task('ci-after-build', async () => del.sync([`!${CURRENT_DEPLOYMENT_FOLDER}/assets`, `!${CURRENT_DEPLOYMENT_FOLDER}/index.html`]));
 
-gulp.task('build', gulp.series('assets', gulp.parallel('pug', 'sass', 'js')));
+gulp.task('build', gulp.series('clean', 'assets', gulp.parallel('pug', 'sass', 'js')));
 
 gulp.task('define-dev-folder', gulp.series(defineDeploymentFolder(DEV_DEPLOYMENT_FOLDER)));
 
 gulp.task('define-ci-folder', gulp.series(defineDeploymentFolder(CI_DEPLOYMENT_FOLDER)));
 
-gulp.task('dev', gulp.series('define-dev-folder', 'dev-clean', 'build'));
+gulp.task('dev', gulp.series('define-dev-folder', 'build'));
 
-gulp.task('ci', gulp.series('define-ci-folder', 'ci-clean', 'build'));
+gulp.task('ci', gulp.series('define-ci-folder', 'build', 'ci-after-build'));

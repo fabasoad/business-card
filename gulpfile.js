22ee414fecfe703d0f9fd785da162f5ba5773e2f
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-06-06 23:29:39 +0900

    Changing .travis.yml file.

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,41 @@ const plumber = require('gulp-plumber');
 const gutil = require('gulp-util');
 const babel = require('gulp-babel');
 
+const CI_DEPLOYMENT_FOLDER = '.';
+const DEV_DEPLOYMENT_FOLDER = 'docs';
+
+let CURRENT_DEPLOYMENT_FOLDER;
+
+const cleanTask = {};
+cleanTask[CI_DEPLOYMENT_FOLDER] = 'ci-clean';
+cleanTask[DEV_DEPLOYMENT_FOLDER] = 'dev-clean';
+
 gulp.task('pug', async () => gulp.src('src/pug/*.pug')
     .pipe(plumber())
     .pipe(pug())
     .on('error', gutil.log)
-    .pipe(gulp.dest('.')));
+    .pipe(gulp.dest(CURRENT_DEPLOYMENT_FOLDER)));
 
 gulp.task('js', async () => gulp.src('src/js/*.js')
     .pipe(plumber())
     .pipe(babel({presets: ['@babel/env']}))
     .on('error', gutil.log)
-    .pipe(gulp.dest('assets/js')));
+    .pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets/js`)));
 
 gulp.task('sass', async () => gulp.src('src/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .on('error', gutil.log)
-    .pipe(gulp.dest('assets/css')));
+    .pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets/css`)));
 
-gulp.task('assets', async () => gulp.src('src/assets/**/*').pipe(gulp.dest('assets')));
+gulp.task('assets', async () => gulp.src('src/assets/**/*').pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets`)));
 
-gulp.task('clean', async () => del.sync(['assets', 'index.html']));
+gulp.task('dev-clean', async () => del.sync([`${CURRENT_DEPLOYMENT_FOLDER}/assets`, `${CURRENT_DEPLOYMENT_FOLDER}/index.html`]));
 
-gulp.task('default', gulp.series('clean', 'assets', gulp.parallel('pug', 'sass', 'js')));
+gulp.task('ci-clean', async () => del.sync(['**']));
+
+gulp.task('build', gulp.series(cleanTask[CURRENT_DEPLOYMENT_FOLDER], 'assets', gulp.parallel('pug', 'sass', 'js')));
+
+gulp.task('dev', gulp.series(() => CURRENT_DEPLOYMENT_FOLDER = CI_DEPLOYMENT_FOLDER, 'build'));
+
+gulp.task('ci', gulp.series(() => CURRENT_DEPLOYMENT_FOLDER = DEV_DEPLOYMENT_FOLDER, 'build'));

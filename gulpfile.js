5f0bc04e6274359b2a8390dd9c640da39f03fc21
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-06-07 22:48:40 +0900

    Adding fancy-log.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
+const log = require('fancy-log');
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 const del = require('del');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
-const gutil = require('gulp-util');
 const babel = require('gulp-babel');
 require('dotenv').config()
 
@@ -14,38 +14,46 @@ let CURRENT_DEPLOYMENT_FOLDER;
 
 const defineDeploymentFolder = f => cb => {
     CURRENT_DEPLOYMENT_FOLDER = f;
-    console.log(`Deployment folder: ${CURRENT_DEPLOYMENT_FOLDER}`);
+    log(`Deployment folder: ${CURRENT_DEPLOYMENT_FOLDER}`);
     cb();
 };
 
 gulp.task('pug', async () => gulp.src('src/pug/*.pug')
     .pipe(plumber())
     .pipe(pug())
-    .on('error', gutil.log)
+    .on('error', log)
     .pipe(gulp.dest(CURRENT_DEPLOYMENT_FOLDER)));
 
 gulp.task('js', async () => gulp.src('src/js/*.js')
     .pipe(plumber())
     .pipe(babel({presets: ['@babel/env']}))
-    .on('error', gutil.log)
+    .on('error', log)
     .pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets/js`)));
 
 gulp.task('sass', async () => gulp.src('src/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sass())
-    .on('error', gutil.log)
+    .on('error', log)
     .pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets/css`)));
 
 gulp.task('assets', async () => gulp.src('src/assets/**/*').pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets`)));
 
-gulp.task('clean', async cb => del.sync([`${CURRENT_DEPLOYMENT_FOLDER}/assets`, `${CURRENT_DEPLOYMENT_FOLDER}/index.html`], cb));
+gulp.task('clean', async cb => {
+    const rules = [`${CURRENT_DEPLOYMENT_FOLDER}/assets`, `${CURRENT_DEPLOYMENT_FOLDER}/index.html`]
+    log(`[clean] Rules for del.sync: ${rules}`);
+    return del.sync(rules, cb);
+});
 
-gulp.task('ci-after-build', async cb => del.sync([
-    `${CURRENT_DEPLOYMENT_FOLDER}/**`,
-    `!${CURRENT_DEPLOYMENT_FOLDER}`,
-    `!${CURRENT_DEPLOYMENT_FOLDER}/assets`,
-    `!${CURRENT_DEPLOYMENT_FOLDER}/index.html`
-], cb));
+gulp.task('ci-after-build', async cb => {
+    const rules = [
+        `${CURRENT_DEPLOYMENT_FOLDER}/**`,
+        `!${CURRENT_DEPLOYMENT_FOLDER}`,
+        `!${CURRENT_DEPLOYMENT_FOLDER}/assets`,
+        `!${CURRENT_DEPLOYMENT_FOLDER}/index.html`
+    ];
+    log(`[ci-after-build] Rules for del.sync: ${rules}`);
+    return del.sync(rules, cb);
+});
 
 gulp.task('build', gulp.series('clean', 'assets', gulp.parallel('pug', 'sass', 'js')));
 

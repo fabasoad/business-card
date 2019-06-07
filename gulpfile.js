38d0f9f5db17f48d74bbbb6dd713d314bfaaae68
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-06-07 23:19:02 +0900

    Changing gulpfile.js file.

--- gulpfile.js
+++ gulpfile.js
@@ -12,39 +12,39 @@ const DEV_DEPLOYMENT_FOLDER = 'docs';
 
 let CURRENT_DEPLOYMENT_FOLDER;
 
-const defineDeploymentFolder = f => cb => {
+const defineDeploymentFolder = f => () => {
     CURRENT_DEPLOYMENT_FOLDER = f;
     log(`Deployment folder: ${CURRENT_DEPLOYMENT_FOLDER}`);
     cb();
 };
 
-gulp.task('pug', async () => gulp.src('src/pug/*.pug')
+gulp.task('pug', () => gulp.src('src/pug/*.pug')
     .pipe(plumber())
     .pipe(pug())
     .on('error', log)
     .pipe(gulp.dest(CURRENT_DEPLOYMENT_FOLDER)));
 
-gulp.task('js', async () => gulp.src('src/js/*.js')
+gulp.task('js', () => gulp.src('src/js/*.js')
     .pipe(plumber())
     .pipe(babel({presets: ['@babel/env']}))
     .on('error', log)
     .pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets/js`)));
 
-gulp.task('sass', async () => gulp.src('src/scss/**/*.scss')
+gulp.task('sass', () => gulp.src('src/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .on('error', log)
     .pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets/css`)));
 
-gulp.task('assets', async () => gulp.src('src/assets/**/*').pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets`)));
+gulp.task('assets', () => gulp.src('src/assets/**/*').pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets`)));
 
-gulp.task('clean', async cb => {
+gulp.task('clean', () => {
     const rules = [`${CURRENT_DEPLOYMENT_FOLDER}/assets`, `${CURRENT_DEPLOYMENT_FOLDER}/index.html`]
     log(`[clean] Rules for del.sync: ${rules}`);
-    return del.sync(rules, cb);
+    return del(rules);
 });
 
-gulp.task('ci-after-build', async cb => {
+gulp.task('ci-after-build', () => {
     const rules = [
         `${CURRENT_DEPLOYMENT_FOLDER}/**`,
         `!${CURRENT_DEPLOYMENT_FOLDER}`,
@@ -52,14 +52,14 @@ gulp.task('ci-after-build', async cb => {
         `!${CURRENT_DEPLOYMENT_FOLDER}/index.html`
     ];
     log(`[ci-after-build] Rules for del.sync: ${rules}`);
-    return del.sync(rules, cb);
+    return del(rules, cb);
 });
 
-gulp.task('build', gulp.series('clean', 'assets', gulp.parallel('pug', 'sass', 'js')));
+gulp.task('build', gulp.series('clean', gulp.parallel('assets', 'pug', 'sass', 'js')));
 
-gulp.task('define-dev-folder', gulp.series(defineDeploymentFolder(DEV_DEPLOYMENT_FOLDER)));
+gulp.task('define-dev-folder', defineDeploymentFolder(DEV_DEPLOYMENT_FOLDER));
 
-gulp.task('define-ci-folder', gulp.series(defineDeploymentFolder(CI_DEPLOYMENT_FOLDER)));
+gulp.task('define-ci-folder', defineDeploymentFolder(CI_DEPLOYMENT_FOLDER));
 
 gulp.task('dev', gulp.series('define-dev-folder', 'build'));
 

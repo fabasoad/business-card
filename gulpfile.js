a7716c904f6bd3701cd703a8eed85e41f9c8cc26
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-06-07 23:31:47 +0900

    Fixing issue.

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ const plumber = require('gulp-plumber');
 const babel = require('gulp-babel');
 require('dotenv').config()
 
-const CI_DEPLOYMENT_FOLDER = process.env.TRAVIS_BUILD_DIR;
-const DEV_DEPLOYMENT_FOLDER = 'docs';
+const CI_BUILD_DIR = process.env.TRAVIS_BUILD_DIR;
+const DEV_BUILD_DIR = 'docs';
 
-let CURRENT_DEPLOYMENT_FOLDER;
+let BUILD_DIR;
 
 const defineDeploymentFolder = f => cb => {
-    CURRENT_DEPLOYMENT_FOLDER = f;
-    log(`Deployment folder: ${CURRENT_DEPLOYMENT_FOLDER}`);
+    BUILD_DIR = f;
+    log(`BUILD_DIR=${BUILD_DIR}`);
     cb();
 };
 
@@ -22,34 +22,34 @@ gulp.task('pug', () => gulp.src('src/pug/*.pug')
     .pipe(plumber())
     .pipe(pug())
     .on('error', log)
-    .pipe(gulp.dest(CURRENT_DEPLOYMENT_FOLDER)));
+    .pipe(gulp.dest(BUILD_DIR)));
 
 gulp.task('js', () => gulp.src('src/js/*.js')
     .pipe(plumber())
     .pipe(babel({presets: ['@babel/env']}))
     .on('error', log)
-    .pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets/js`)));
+    .pipe(gulp.dest(`${BUILD_DIR}/assets/js`)));
 
 gulp.task('sass', () => gulp.src('src/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .on('error', log)
-    .pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets/css`)));
+    .pipe(gulp.dest(`${BUILD_DIR}/assets/css`)));
 
-gulp.task('assets', () => gulp.src('src/assets/**/*').pipe(gulp.dest(`${CURRENT_DEPLOYMENT_FOLDER}/assets`)));
+gulp.task('assets', () => gulp.src('src/assets/**/*').pipe(gulp.dest(`${BUILD_DIR}/assets`)));
 
 gulp.task('clean', () => {
-    const rules = [`${CURRENT_DEPLOYMENT_FOLDER}/assets`, `${CURRENT_DEPLOYMENT_FOLDER}/index.html`]
+    const rules = [`${BUILD_DIR}/assets`, `${BUILD_DIR}/index.html`]
     log(`[clean] del(${rules})`);
     return del(rules);
 });
 
 gulp.task('ci-after-build', () => {
     const rules = [
-        `${CURRENT_DEPLOYMENT_FOLDER}/**`,
-        `!${CURRENT_DEPLOYMENT_FOLDER}`,
-        `!${CURRENT_DEPLOYMENT_FOLDER}/assets`,
-        `!${CURRENT_DEPLOYMENT_FOLDER}/index.html`
+        `${BUILD_DIR}/**`,
+        `!${BUILD_DIR}`,
+        `!${BUILD_DIR}/assets/**`,
+        `!${BUILD_DIR}/index.html`
     ];
     log(`[ci-after-build] del(${rules})`);
     return del(rules);
@@ -57,9 +57,9 @@ gulp.task('ci-after-build', () => {
 
 gulp.task('build', gulp.series('clean', gulp.parallel('assets', 'pug', 'sass', 'js')));
 
-gulp.task('define-dev-folder', defineDeploymentFolder(DEV_DEPLOYMENT_FOLDER));
+gulp.task('define-dev-folder', defineDeploymentFolder(DEV_BUILD_DIR));
 
-gulp.task('define-ci-folder', defineDeploymentFolder(CI_DEPLOYMENT_FOLDER));
+gulp.task('define-ci-folder', defineDeploymentFolder(CI_BUILD_DIR));
 
 gulp.task('dev', gulp.series('define-dev-folder', 'build'));
 

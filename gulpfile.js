df28f096706668fc6f97321950dccab4b7bc33fc
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-06-07 22:01:43 +0900

    Changing gulpfile.js file.

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,12 @@ gulp.task('assets', async () => gulp.src('src/assets/**/*').pipe(gulp.dest(`${CU
 
 gulp.task('clean', async cb => del.sync([`${CURRENT_DEPLOYMENT_FOLDER}/assets`, `${CURRENT_DEPLOYMENT_FOLDER}/index.html`], cb));
 
-gulp.task('ci-after-build', async cb => del.sync([`${CURRENT_DEPLOYMENT_FOLDER}/**/*`, `!${CURRENT_DEPLOYMENT_FOLDER}/assets`, `!${CURRENT_DEPLOYMENT_FOLDER}/index.html`], cb));
+gulp.task('ci-after-build', async cb => del.sync([
+    `${CURRENT_DEPLOYMENT_FOLDER}/**`,
+    `!${CURRENT_DEPLOYMENT_FOLDER}`,
+    `!${CURRENT_DEPLOYMENT_FOLDER}/assets`,
+    `!${CURRENT_DEPLOYMENT_FOLDER}/index.html`
+], cb));
 
 gulp.task('build', gulp.series('clean', 'assets', gulp.parallel('pug', 'sass', 'js')));
 

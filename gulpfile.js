1cdc13acdb096bd240aeb7683fb7c8fff8b3f4e6
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-12-15 17:19:23 +0900

    Fix CI job

--- gulpfile.js
+++ gulpfile.js
@@ -42,10 +42,10 @@ gulp.task('clean:ci', () => {
         `${BUILD_DIR}/**`,
         `!${BUILD_DIR}`,
         `!${BUILD_DIR}/assets/**`,
-        `!${BUILD_DIR}/i18n/**`,
+        `!${BUILD_DIR}/i18n`,
         `!${BUILD_DIR}/index.html`
     ];
-    log(`Processing 'ci-after-build'. Deleting ${rules}...`);
+    log(`Deleting ${rules}...`);
     return del(rules);
 });
 

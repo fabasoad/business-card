751255bf58af06e62704b1fa34e60dbd2c7d3c1e
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-05-07 16:42:33 +0900

    #29 - Fix test

--- src/__tests__/scripts/technologies/TechnologyStorage.spec.ts
+++ src/__tests__/scripts/technologies/TechnologyStorage.spec.ts
@@ -1,38 +1,21 @@
 import TechnologyStorage from '../../../scripts/technologies/TechnologyStorage'
-import {
-  Technology,
-  TechnologyProvider
-} from '../../../scripts/technologies/types'
+import { Technology } from '../../../scripts/technologies/types'
 
 let technologyStorage: TechnologyStorage
-let techs: Array<Technology>
-
-class TechnologyProviderStub implements TechnologyProvider {
-  items: Map<string, Technology>
-
-  constructor(techArr: Array<Technology>) {
-    this.items = techArr.reduce(
-      (map: Map<string, Technology>, t: Technology) => {
-        map.set(t.name, t)
-        return map
-      },
-      new Map<string, Technology>()
-    )
-  }
-}
 
 beforeAll(() => {
-  techs = new Array<Technology>(
-    { name: 'tech-1', isSkill: true },
-    { name: 'tech-2', isSkill: false }
-  )
-  technologyStorage = new TechnologyStorage(new TechnologyProviderStub(techs))
+  technologyStorage = new TechnologyStorage({
+    items: new Map<string, Technology>([
+      ['tech-1', { name: 'tech-1', isSkill: true }],
+      ['tech-2', { name: 'tech-2', isSkill: false }]
+    ])
+  })
 })
 
 test('findByName should works correctly if items are present', () => {
-  const actual = technologyStorage.findByName(techs[0].name)
-  expect(actual.name).toBe(techs[0].name)
-  expect(actual.isSkill).toBe(techs[0].isSkill)
+  const actual = technologyStorage.findByName('tech-1')
+  expect(actual.name).toBe('tech-1')
+  expect(actual.isSkill).toBe(true)
 })
 
 test('findByName should works correctly if items are not present', () => {
@@ -45,14 +28,16 @@ test('findByName should works correctly if items are not present', () => {
 test('findBySkill should works correctly if items are present', () => {
   const actual: Technology[] = technologyStorage.findBySkill(false)
   expect(actual.length).toBe(1)
-  expect(actual[0].name).toBe(techs[1].name)
-  expect(actual[0].isSkill).toBe(techs[1].isSkill)
+  expect(actual[0].name).toBe('tech-2')
+  expect(actual[0].isSkill).toBe(false)
 })
 
 test('findBySkill should works correctly if items are not present', () => {
-  const ts = new TechnologyStorage(
-    new TechnologyProviderStub([{ name: 'tech-3', isSkill: false }])
-  )
+  const ts = new TechnologyStorage({
+    items: new Map<string, Technology>([
+      ['tech-3', { name: 'tech-3', isSkill: false }]
+    ])
+  })
   const actual: Technology[] = ts.findBySkill(true)
   expect(actual).not.toBeNull()
   expect(actual.length).toBe(0)

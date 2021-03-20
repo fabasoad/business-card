02e27b04e10ee6c870655de827cbed13f0105c09
fabasoad <fabasoad@gmail.com>
2021-03-20 11:06:20 +0900

    Add job location

--- src/__tests__/components/Experience/JobTitle.spec.tsx
+++ src/__tests__/components/Experience/JobTitle.spec.tsx
@@ -4,6 +4,12 @@ import { shallow } from 'enzyme'
 import JobTitle from '../../../components/Experience/JobTitle'
 
 test('should render JobTitle correctly', () => {
-  const wrapper = shallow(<JobTitle company="test-company" title="test-title" />)
+  const wrapper = shallow(
+    <JobTitle
+      company="test-company"
+      title="test-title"
+      location="test-location"
+    />
+  )
   expect(wrapper).toMatchSnapshot()
 })
--- src/components/Experience/JobTitle.tsx
+++ src/components/Experience/JobTitle.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 interface JobTitleProps {
   company: string
   title: string
+  location: string
 }
 
 export default function JobTitle(props: JobTitleProps) {
@@ -10,6 +11,7 @@ export default function JobTitle(props: JobTitleProps) {
     <div className="timeline-heading">
       <h4>{props.company}</h4>
       <h4 className="subheading">{props.title}</h4>
+      <h6>{props.location}</h6>
     </div>
   )
 }

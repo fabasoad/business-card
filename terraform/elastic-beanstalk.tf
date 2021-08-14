e26a3f79342e87ed4981e9ec0d4085af72d5c14b
fabasoad <fabasoad@gmail.com>
2021-08-14 12:38:54 +0900

    Format terraform files

--- terraform/elastic-beanstalk.tf
+++ terraform/elastic-beanstalk.tf
@@ -18,7 +18,7 @@ resource "aws_s3_bucket_object" "business-card-payload" {
   bucket = aws_s3_bucket.business-card-bucket.id
   key    = "beanstalk/${var.app}-payload.zip"
   source = local.payload_path
-  etag = md5(file(local.payload_path))
+  etag   = md5(file(local.payload_path))
 }
 
 resource "aws_elastic_beanstalk_application_version" "default" {

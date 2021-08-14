358c49f587a5ec3479d809b7d5708bb6d1a36309
fabasoad <fabasoad@gmail.com>
2021-08-14 12:59:44 +0900

    Fix path

--- server/build_bundle.sh
+++ server/build_bundle.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 app_name='business-card'
+
+cd ..
 rm -f ${app_name}-payload.zip
 
-echo '[1/4] Installing latest server dependencies'
-npm install
-cd ..
-
-echo '[2/4] Building latest website bundle'
+echo '[1/4] Building latest website bundle'
 yarn run build:prod
 
-echo '[3/4] Zipping bundle'
+echo '[2/4] Zipping bundle'
 zip -r ${app_name}-payload.zip public
 mv ${app_name}-payload.zip server/${app_name}-payload.zip
 cd server || exit
 
+echo '[3/4] Installing latest server dependencies'
+npm install
+
 echo '[4/4] Zipping server'
 zip -r ${app_name}-payload.zip node_modules package.json package-lock.json web.js
+mv ${app_name}-payload.zip ../${app_name}-payload.zip
 
 echo 'Done!'
---
+++ terraform/variables.auto.tfvars
@@ -0,0 +1,1 @@
+payload_path = "../business-card-payload.zip"

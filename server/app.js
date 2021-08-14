0e66f2bd66c3ffc097ea3966f20ae63c2e9f4f5b
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2021-08-14 17:33:34 +0900

    Add "Trigger deployment" workflow (#95)
    * Add terraform files
    
    * Change lambda to elastic-beanstalk
    
    * Add 'Prepare bundle' step to CI
    
    * Fix path in CI
    
    * Format terraform files
    
    * Playing with upload artifact
    
    * Add 'needs' to 2nd job
    
    * Fix path
    
    * Fix path
    
    * Fix script
    
    * Fix script
    
    * Fix workflow syntax
    
    * Remove 'Move bundle' step
    
    * Try to deploy
    
    * Add Route 53 terraform
    
    * Fix CI
    
    * Move tf files to separate repos
    
    * Use secrets
    
    * Remove branches: test-* condition

---
+++ server/app.js
@@ -0,0 +1,24 @@
+// Server production build locally
+
+const path = require('path');
+const express = require('express');
+
+const PUBLIC_DIR = path.join(__dirname, process.env.PUBLIC_PATH || 'public');
+const PORT = 8080;
+const app = express();
+
+const RateLimit = require('express-rate-limit');
+const limiter = new RateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 150,
+});
+
+app.use(limiter);
+
+app.use(express.static(PUBLIC_DIR));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(PUBLIC_DIR, 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server is up and running on ${PORT} port...`));

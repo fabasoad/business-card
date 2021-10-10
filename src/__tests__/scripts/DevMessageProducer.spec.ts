099d50e846240466b14e40ce5b9f900854ed8ef8
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2021-10-10 22:19:29 +0900

    Bump babel-jest from 27.0.6 to 27.2.5 (#120)
    * Bump babel-jest from 27.0.6 to 27.2.5
    
    * Remove some tests
    
    * Increase patch version

--- src/__tests__/scripts/DevMessageProducer.spec.ts
+++ src/__tests__/scripts/DevMessageProducer.spec.ts
@@ -1,40 +1,26 @@
 import DevMessageProducer from '../../scripts/DevMessageProducer'
 
-jest.useFakeTimers()
+describe('DevMessageProducer', () => {
+  let consoleLogSpy
 
-let consoleLogSpy
+  beforeAll(() => {
+    jest.useFakeTimers()
+    consoleLogSpy = jest.spyOn(global.console, 'log').mockImplementation()
+  })
 
-beforeAll(() => {
-  consoleLogSpy = jest.spyOn(global.console, 'log').mockImplementation()
-})
+  test('should build default messages correctly', () => {
+    const messages: string[] = new DevMessageProducer()._messages
+    expect(messages.length).toBe(9)
+    new Array<string>(
+      'Open Collective',
+      'Ko-fi',
+      'Liberapay',
+      'IssueHunt',
+      'BTC'
+    ).forEach((m) => expect(messages[messages.length - 1]).toContain<string>(m))
+  })
 
-test('should produce all messages on run', () => {
-  const messages = ['test-1', 'test-2']
-  const producer = new DevMessageProducer(() => messages)
-  producer.run()
-  producer.run() // to be sure that timer will not run again
-  jest.runAllTimers()
-  expect(setInterval).toHaveBeenCalledTimes(1)
-  expect(setInterval).toHaveBeenCalledWith<number[]>(expect.any(Function), 1500)
-  expect(clearInterval).toHaveBeenCalledTimes(1)
-  expect(consoleLogSpy).toHaveBeenCalledTimes(2)
-  messages.forEach((m, i) =>
-    expect(consoleLogSpy).toHaveBeenNthCalledWith<string[]>(i + 1, m)
-  )
-})
-
-test('should build default messages correctly', () => {
-  const messages: string[] = new DevMessageProducer()._messages
-  expect(messages.length).toBe(9)
-  new Array<string>(
-    'Open Collective',
-    'Ko-fi',
-    'Liberapay',
-    'IssueHunt',
-    'BTC'
-  ).forEach((m) => expect(messages[messages.length - 1]).toContain<string>(m))
-})
-
-afterEach(() => {
-  consoleLogSpy.mockRestore()
+  afterEach(() => {
+    consoleLogSpy.mockRestore()
+  })
 })

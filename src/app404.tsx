b527537ef4c3faca58398ab7b8c5132bb96f2969
fabasoad <fabasoad@gmail.com>
2022-06-05 10:15:11 +0900

    Bump react from 17.0.2 to 18.0.1

--- src/app404.tsx
+++ src/app404.tsx
@@ -1,22 +1,18 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-
-import './styles/style404.scss'
-
-import App404 from './components/App404'
-import LoadingSpinner from './components/LoadingSpinner'
-
-import { startLoadLocale } from './store/locale/actions'
-import { store } from './store/configureStore'
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App404 from './components/App404';
+import LoadingSpinner from './components/LoadingSpinner';
+import { store } from './store/configureStore';
+import { startLoadLocale } from './store/locale/actions';
+import './styles/style404.scss';
 
 store.dispatch(startLoadLocale())
 
-ReactDOM.render(
+createRoot(document.getElementById('app')!).render(
   <React.Suspense fallback={<LoadingSpinner />}>
     <Provider store={store}>
       <App404 />
     </Provider>
-  </React.Suspense>,
-  document.getElementById('app')
+  </React.Suspense>
 )

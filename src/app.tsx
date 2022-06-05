41c4836fd94cdab178923325a2ba9cca0f1da907
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2022-06-05 10:19:10 +0900

    Bump react from 17.0.2 to 18.0.1 (#165)

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,23 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-
-import App from './components/App'
-import LoadingSpinner from './components/LoadingSpinner'
-
-import './styles/style.scss'
-
-import './libs/fontawesome'
-import DevMessageProducer from './scripts/DevMessageProducer'
-import { startLoadLocale } from './store/locale/actions'
-import { store } from './store/configureStore'
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import LoadingSpinner from './components/LoadingSpinner';
+import './libs/fontawesome';
+import DevMessageProducer from './scripts/DevMessageProducer';
+import { store } from './store/configureStore';
+import { startLoadLocale } from './store/locale/actions';
+import './styles/style.scss';
 
 const producer = new DevMessageProducer()
 producer.run()
 
 store.dispatch(startLoadLocale())
 
-ReactDOM.render(
+createRoot(document.getElementById('app')!).render(
   <React.Suspense fallback={<LoadingSpinner />}>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.Suspense>,
-  document.getElementById('app')
+  </React.Suspense>
 )

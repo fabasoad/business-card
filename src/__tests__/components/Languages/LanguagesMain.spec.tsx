3ca924509fbc8356c1b28bdbf70a19f574dfeaa8
fabasoad <fabasoad@gmail.com>
2022-06-05 11:41:00 +0900

    Fix tests

--- src/__tests__/components/Languages/LanguagesMain.spec.tsx
+++ src/__tests__/components/Languages/LanguagesMain.spec.tsx
@@ -1,23 +1,21 @@
 /// <reference types="jest" />
-import * as React from 'react'
 import { shallow, ShallowWrapper } from 'enzyme'
+import * as React from 'react'
 import LanguagesMain from '../../../components/Languages/LanguagesMain'
 import { useTranslation } from '../../__mocks__/react-i18next'
 
-let tMock
+describe(LanguagesMain.name, () => {
+  let tMock
 
-beforeAll(() => {
-  tMock = useTranslation().t
-})
+  beforeAll(() => {
+    tMock = useTranslation().t
+  })
 
-test('should render LanguageItem correctly', () => {
-  const wrapper: ShallowWrapper = shallow(<LanguagesMain />)
-  expect(wrapper).toMatchSnapshot()
-  expect(tMock).toHaveBeenCalledTimes(3)
-  expect(tMock).toHaveBeenNthCalledWith(1, 'business-card-languages-title')
-  new Array<number>(2, 3).forEach(
-    (n) => expect(tMock).toHaveBeenNthCalledWith(
-      n, 'business-card-languages-native'
-    )
-  )
+  test('should render LanguageItem correctly', () => {
+    const wrapper: ShallowWrapper = shallow(<LanguagesMain />)
+    expect(wrapper).toMatchSnapshot()
+    expect(tMock).toHaveBeenCalledTimes(2)
+    expect(tMock).toHaveBeenNthCalledWith(1, 'business-card-languages-title')
+    expect(tMock).toHaveBeenNthCalledWith(2, 'business-card-languages-native')
+  })
 })

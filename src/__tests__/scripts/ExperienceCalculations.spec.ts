6c640d474efcf7ba257310a61ae7aa43a510a005
fabasoad <fabasoad@gmail.com>
2022-06-23 23:55:31 +0900

    Fix tests

--- src/__tests__/scripts/ExperienceCalculations.spec.ts
+++ src/__tests__/scripts/ExperienceCalculations.spec.ts
@@ -3,5 +3,10 @@ import ExperienceCalculations from '../../scripts/TotalExperience'
 test(`should calculate total experience correctly`, () => {
   const expected: number = Date.now() - Date.parse('2010-03-01')
   const c = new ExperienceCalculations()
-  expect(c.humanize('gb')).toBe(`${Math.round(expected / 31536000000)} years`)
+  const actual: string[] = c.humanize('gb').split(' ');
+  const x: number = expected / 31536000000;
+  expect(+actual[0]).toBe(Math.round(x))
+  expect(actual[1]).toBe('years')
+  expect(+actual[2]).toBe(Math.ceil((x - Math.round(x)) * 12))
+  expect(actual[3]).toBe('months')
 })

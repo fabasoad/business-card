e1bc0b2eba21fb72d27a702a64fbbf4c07e8b310
fabasoad <fabasoad@gmail.com>
2022-08-14 09:51:15 +0900

    Bump @babel/cli from 7.18.9 to 7.18.10

--- src/scripts/TotalExperience.ts
+++ src/scripts/TotalExperience.ts
@@ -4,10 +4,19 @@ export interface ITotalExperience {
   humanize(code: string): string
 }
 
+type EndDateGenerator = () => Date
+
 export default class TotalExperience implements ITotalExperience {
   private static readonly EXPERIENCE_START_DATE: Date = new Date(2010, 2, 1)
 
+  private readonly endDateGenerator: EndDateGenerator
+
+  constructor(endDateGenerator: EndDateGenerator = () => new Date()) {
+    this.endDateGenerator = endDateGenerator
+  }
+
   public humanize(code: string): string {
-    return DateUtils.humanize(TotalExperience.EXPERIENCE_START_DATE, new Date(), code)
+    return DateUtils.humanize(
+      TotalExperience.EXPERIENCE_START_DATE, this.endDateGenerator(), code)
   }
 }

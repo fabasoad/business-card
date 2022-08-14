32f320ab4e176de21045393d3ac0f15ae5e880fe
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2022-08-14 10:00:40 +0900

    Bump @babel/cli from 7.18.9 to 7.18.10 (#179)
    * Bump @babel/cli from 7.18.9 to 7.18.10
    
    * Remove Snyk tests from GHA

--- src/__tests__/scripts/ExperienceCalculations.spec.ts
+++ src/__tests__/scripts/ExperienceCalculations.spec.ts
@@ -1,12 +1,11 @@
-import ExperienceCalculations from '../../scripts/TotalExperience'
+import TotalExperience from '../../scripts/TotalExperience'
 
 test(`should calculate total experience correctly`, () => {
-  const expected: number = Date.now() - Date.parse('2010-03-01')
-  const c = new ExperienceCalculations()
-  const actual: string[] = c.humanize('gb').split(' ');
-  const x: number = expected / 31536000000;
-  expect(+actual[0]).toBe(Math.round(x))
-  expect(actual[1]).toBe('years')
-  expect(+actual[2]).toBe(Math.ceil((x - Math.round(x)) * 12))
-  expect(actual[3]).toBe('months')
+  const c = new TotalExperience(() => new Date(2012, 6, 1))
+  const actual: string[] = c.humanize('gb').split(' ')
+  expect<number>(actual.length).toBe(4)
+  expect<number>(+actual[0]).toBe(2)
+  expect<string>(actual[1]).toBe('years')
+  expect<number>(+actual[2]).toBe(4)
+  expect<string>(actual[3]).toBe('months')
 })
--- src/components/Footer/FooterInfo.tsx
+++ src/components/Footer/FooterInfo.tsx
@@ -9,7 +9,7 @@ export default function FooterInfo() {
     <div className="social d-flex justify-content-center">
       <ul className="icon-list">
         <li><img src="https://img.shields.io/static/v1?label=made%20by&amp;message=fabasoad&amp;color=2c3e50&amp;style=for-the-badge&amp;logo=github" alt="Made by fabasoad" /></li>
-        <li><a target="_blank" rel="noopener noreferrer" href="https://en.cryptobadges.io/donate/145HwyQAcv4vrzUumJhu7nWGAVBysX9jJH"><img src="https://en.cryptobadges.io/badge/micro/145HwyQAcv4vrzUumJhu7nWGAVBysX9jJH" alt="Donate with Bitcoin" /></a></li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://www.bitcoinqrcodemaker.com/?style=bitcoin&address=145HwyQAcv4vrzUumJhu7nWGAVBysX9jJH&prefix=on"><img src="https://img.shields.io/badge/bitcoin-donate-yellow?style=flat&logo=bitcoin" alt="Donate with Bitcoin" /></a></li>
         <li><Badge pill={true} bg="secondary">{t<string>('business-card-version', { version })}</Badge></li>
       </ul>
     </div>

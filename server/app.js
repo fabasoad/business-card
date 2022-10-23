af448e598f68bc2863341eb972c50de2f0c61bc2
fabasoad <fabasoad@gmail.com>
2022-10-23 23:03:50 +0900

    Bump sass-loader from 13.0.2 to 13.1.0

--- server/app.js
+++ server/app.js
@@ -7,13 +7,12 @@ const PUBLIC_DIR = path.join(__dirname, process.env.PUBLIC_PATH || 'public');
 const PORT = 8080;
 const app = express();
 
-const RateLimit = require('express-rate-limit');
-const limiter = new RateLimit({
+const limiter = require('express-rate-limit');
+
+app.use(limiter({
   windowMs: 3 * 1000,
   max: 150,
-});
-
-app.use(limiter);
+}));
 
 app.use(express.static(PUBLIC_DIR));
 

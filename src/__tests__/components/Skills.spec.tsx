10143a3f55f4d82aea197eae4648575be0c3759e
fabasoad <fabasoad@gmail.com>
2022-12-05 21:48:02 +0900

    Migrate Skills.spec.tsx from enzyme to react testing library

--- src/__tests__/components/Skills.spec.tsx
+++ src/__tests__/components/Skills.spec.tsx
@@ -1,31 +1,42 @@
-/// <reference types="jest" />
 import * as React from 'react'
-import { shallow, ShallowWrapper } from 'enzyme'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
 import Skills from '../../components/Skills'
-import { useTranslation } from '../__mocks__/react-i18next'
-import { Technology } from '../../scripts/technologies/types'
-import TechnologyStorage from '../../scripts/technologies/TechnologyStorage'
-
-let tMock
-
-beforeAll(() => {
-  tMock = useTranslation().t
-})
 
 test('should render Skills correctly', () => {
-  const findBySkillSpy: () => Array<Technology> = jest.fn(
-    () => new Array<number>(1, 2).map(
-      (n) => ({ name: `name-${n}`, title: `title-${n}`, img: `img-${n}`, isSkill: true })
-    )
-  )
-  const technologyStorage = new TechnologyStorage(null)
-  technologyStorage.findBySkill = findBySkillSpy
-  const wrapper: ShallowWrapper = shallow(
-    <Skills technologyStorage={technologyStorage} />
-  )
-  expect(wrapper).toMatchSnapshot()
-  expect(tMock).toHaveBeenCalledTimes(1)
-  expect(tMock).toHaveBeenCalledWith('business-card-skills-title')
-  expect(findBySkillSpy).toHaveBeenCalledTimes(1)
-  expect(findBySkillSpy).toHaveBeenCalledWith(true)
+  const { container } = render(<Skills />)
+  expect(screen.getByRole('heading', { level: 2 }))
+    .toHaveTextContent('business-card-skills-title')
+  expect(container.querySelectorAll('img')).toHaveLength(22)
+
+  const expectedSkills = [
+    { alt: 'aws', title: 'AWS' },
+    { alt: 'bootstrap', title: 'Bootstrap' },
+    { alt: 'concourseCI', title: 'Concourse CI' },
+    { alt: 'csharp', title: 'C#' },
+    { alt: 'docker', title: 'Docker' },
+    { alt: 'dotNetCore', title: '.NET Core' },
+    { alt: 'git', title: 'Git' },
+    { alt: 'gitHub', title: 'GitHub' },
+    { alt: 'gitHubActions', title: 'GitHub Actions' },
+    { alt: 'gitlab', title: 'GitLab' },
+    { alt: 'gitlabCi', title: 'GitLab CI' },
+    { alt: 'java', title: 'Java' },
+    { alt: 'javaScript', title: 'JavaScript' },
+    { alt: 'kubernetes', title: 'Kubernetes' },
+    { alt: 'maven', title: 'Maven' },
+    { alt: 'mongodb', title: 'MongoDB' },
+    { alt: 'python', title: 'Python' },
+    { alt: 'terraform', title: 'Terraform' },
+    { alt: 'travis', title: 'Travis CI' },
+    { alt: 'typeScript', title: 'TypeScript' },
+    { alt: 'xamarin', title: 'Xamarin' }
+  ]
+
+  for (const { alt, title } of expectedSkills) {
+    const img = screen.getByAltText(alt);
+    expect(img).toHaveAttribute('title', title)
+    expect(img).toHaveAttribute('src', '[object Object]')
+  }
 })

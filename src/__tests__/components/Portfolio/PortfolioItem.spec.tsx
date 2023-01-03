698b8fdb69f8e1c34ac9e1abe8c49f5e7a476c92
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2023-01-04 01:25:25 +0900

    Bump eslint from 8.30.0 to 8.31.0 (#218)
    - Bump eslint from 8.30.0 to 8.31.0
    - Migrate a few components from enzyme to react testing library
    - Update LICENSE
    - Add `Update License` workflow

--- src/__tests__/components/Portfolio/PortfolioItem.spec.tsx
+++ src/__tests__/components/Portfolio/PortfolioItem.spec.tsx
@@ -1,15 +1,27 @@
-/// <reference types="jest" />
+import '@testing-library/jest-dom'
 import * as React from 'react'
-import { shallow, ShallowWrapper } from 'enzyme'
+import { render } from '@testing-library/react'
+
 import PortfolioItem from '../../../components/Portfolio/PortfolioItem'
+import { testPortfolioItem } from './TestUtils'
 
 test('should render PortfolioItem correctly', () => {
-  const wrapper: ShallowWrapper = shallow(<PortfolioItem
-    img="test-img"
-    name="test-name"
-    subtitle="test-subtitle"
-    title="test-title"
-    url="test-url"
-  />)
-  expect(wrapper).toMatchSnapshot()
+  const img = 'test-img'
+  const name = 'test-name'
+  const url = 'test-url'
+  const title = 'test-title'
+  const subtitle = 'test-subtitle'
+  const { container } = render(
+    <PortfolioItem
+      img={img}
+      name={name}
+      subtitle={subtitle}
+      title={title}
+      url={url}
+    />
+  )
+  testPortfolioItem(
+    container.querySelector('div.col-sm-6.col-md-3.col-lg-3'),
+    url, name, title, subtitle, img
+  )
 })
--- src/scripts/services/LeetcodeService.ts
+++ src/scripts/services/LeetcodeService.ts
@@ -12,17 +12,11 @@ export type LeetcodeLoaded = (stats: LeetcodeStats) => void
 export default class LeetcodeService {
   private static instance: LeetcodeService
   private state: State = State.NOT_STARTED
-  private readonly stats: LeetcodeStats
+  private stats: LeetcodeStats
 
   private static LEETCODE_USERNAME: string = 'fabasoad'
 
   private constructor() {
-    this.stats = {
-      totalSolved: 171,
-      easySolved: 81,
-      mediumSolved: 88,
-      hardSolved: 2
-    }
   }
 
   public static getInstance(): LeetcodeService {
@@ -33,19 +27,18 @@ export default class LeetcodeService {
   }
 
   public async getStats(): Promise<LeetcodeStats> {
-    // TODO: replace with working code
-    // if (this.state === State.NOT_STARTED || this.state === State.FAILED) {
-    //   this.state = State.STARTED
-    //   return fetch(`https://leetcode-stats-api.herokuapp.com/${LeetcodeService.LEETCODE_USERNAME}`)
-    //     .then((resp) => resp.json())
-    //     .then(({ totalSolved, easySolved, mediumSolved, hardSolved }) => this.stats = {
-    //       totalSolved, easySolved, mediumSolved, hardSolved
-    //     })
-    //     .catch(() => {
-    //       this.state = State.FAILED
-    //       return this.stats
-    //     })
-    // }
+    if (this.state === State.NOT_STARTED || this.state === State.FAILED) {
+      this.state = State.STARTED
+      return fetch(`https://leetcode-stats-api.herokuapp.com/${LeetcodeService.LEETCODE_USERNAME}`)
+        .then((resp) => resp.json())
+        .then(({ totalSolved, easySolved, mediumSolved, hardSolved }) => this.stats = {
+          totalSolved, easySolved, mediumSolved, hardSolved
+        })
+        .catch(() => {
+          this.state = State.FAILED
+          return this.stats
+        })
+    }
     return Promise.resolve(this.stats)
   }
 }

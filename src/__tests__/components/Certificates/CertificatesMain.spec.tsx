7501be15e80608d40cf0df5ae1908b1773fc727a
fabasoad <fabasoad@gmail.com>
2024-12-29 18:43:22 +0900

    Bump version to 2.12.0

--- src/__tests__/components/Certificates/CertificatesMain.spec.tsx
+++ src/__tests__/components/Certificates/CertificatesMain.spec.tsx
@@ -1,13 +1,12 @@
 import '@testing-library/jest-dom'
 import * as React from 'react'
 import CertificatesMain from '../../../components/Certificates/CertificatesMain'
-import SupportedLocales from '../../../scripts/i18n/SupportedLocales'
 import { render } from '@testing-library/react'
 import { testCertificatesMain } from './TestUtils'
 
 describe('CertificatesMain', () => {
   test('should render CertificatesMain correctly', () => {
     const { container } = render(<CertificatesMain/>)
-    testCertificatesMain(container.querySelector('div#certificates'), SupportedLocales.default)
+    testCertificatesMain(container.querySelector('div#certificates'))
   })
 })
--- src/__tests__/components/Certificates/TestUtils.ts
+++ src/__tests__/components/Certificates/TestUtils.ts
@@ -8,7 +8,6 @@ export function testCertificateItem(
   locale: Locale,
   date: Date,
   issuerName: string,
-  technology: string,
   name: string,
   url: string
 ) {
@@ -22,15 +21,18 @@ export function testCertificateItem(
   expect(elements).toHaveLength(1)
   expect(elements[0]).toHaveAttribute('src', '[object Object]')
 
-  elements = div.getElementsByClassName('card-header')
+  elements = div.getElementsByClassName('card-title')
   expect(elements).toHaveLength(1)
-  expect(elements[0]).toHaveTextContent(issuer.name)
 
-  elements = div.getElementsByClassName('card-link')
+  elements = elements[0].getElementsByClassName('card-link')
   expect(elements).toHaveLength(1)
   const cardLink = expect(elements[0])
   cardLink.toHaveAttribute('href', url)
   cardLink.toHaveTextContent(name)
+
+  elements = div.getElementsByClassName('card-subtitle')
+  expect(elements).toHaveLength(1)
+  expect(elements[0]).toHaveTextContent(`Issuer: ${issuer.name}`)
 
   elements = div.getElementsByClassName('card-footer')
   expect(elements).toHaveLength(1)
@@ -41,15 +43,7 @@ export function testCertificateItem(
   )
 }
 
-export function testCertificatesMain(div: HTMLDivElement, locale: Locale) {
-  function findByUrl(items: Array<any>, url: string): any {
-    for (const item of items) {
-      if (item.url === url) {
-        return item
-      }
-    }
-    return null
-  }
+export function testCertificatesMain(div: HTMLDivElement) {
   expect(div).toHaveClass('light-component')
   testSectionTitle(
     div.querySelector('div.section-title'),
@@ -59,27 +53,10 @@ export function testCertificatesMain(div: HTMLDivElement, locale: Locale) {
   expect(divContainer).not.toBeNull()
   const divRow = divContainer.querySelector('div.row')
   expect(divRow).toHaveClass('row-cols-2')
-  expect(divRow).toHaveClass('row-cols-md-3')
-  expect(divRow).toHaveClass('row-cols-lg-5')
-  const divColElements = divRow.querySelectorAll('div.col-lg')
-  // TODO: Test new certificates items
-  // expect(divColElements).toHaveLength(items.length)
-  // for (let ind = 0; ind < items.length; ind++) {
-  //   const divCol = divColElements.item(ind)
-  //   expect(divCol).toHaveClass('mb-2')
-  //   const divCard: HTMLDivElement = divCol.querySelector('div.card')
-  //   const divCardLinks = divCard.getElementsByClassName('card-link')
-  //   expect(divCardLinks).toHaveLength(1)
-  //   const item = findByUrl(items, divCardLinks.item(0).getAttribute('href'))
-  //   const name = item.i18nTitleKey
-  //   testCertificateItem(
-  //     divCard,
-  //     locale,
-  //     new Date(item.date),
-  //     item.issuer,
-  //     item.technology,
-  //     name,
-  //     item.url
-  //   )
-  // }
+  expect(divRow).toHaveClass('row-cols-sm-3')
+  expect(divRow).toHaveClass('row-cols-md-4')
+  expect(divRow).toHaveClass('row-cols-lg-6')
+  expect(divRow.querySelectorAll('div.col')).toHaveLength(11)
+  expect(divRow.querySelectorAll('div.col div[data-share-badge-host="https://www.credly.com"]')).toHaveLength(7)
+  expect(divRow.querySelectorAll('div.col div.card')).toHaveLength(4)
 }

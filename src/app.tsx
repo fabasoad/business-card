f400347603cfc4ce25a10683fb32a26a90d4be12
fabasoad <fabasoad@gmail.com>
2025-01-11 00:01:23 +0900

    Delete .env

--- src/app.tsx
+++ src/app.tsx
@@ -4,11 +4,24 @@ import App from './components/App'
 import AppWrapper from './components/AppWrapper'
 import LoadingSpinner from './components/LoadingSpinner'
 import { createRoot } from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+}
+
+console.log('aaaa' + process.env.REACT_APP_PUBLIC_POSTHOG_HOST)
+console.log(process.env.REACT_APP_PUBLIC_POSTHOG_KEY)
 
 createRoot(document.getElementById('app')!).render(
   <React.Suspense fallback={<LoadingSpinner />}>
     <AppWrapper>
-      <App />
+      <PostHogProvider
+        apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+        options={options}
+      >
+        <App />
+      </PostHogProvider>
     </AppWrapper>
   </React.Suspense>
 )
